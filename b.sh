eefe9fdb0c686c45c4a9411bc9fdfa20128a53b2
GlobalAwear <awear@appsyouwear.com>
2021-07-22 15:13:51 +0200

    clean

--- b.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git add .
-git commit -m "bla"
-git tag $1
-git push origin $1
-
-
--- create_bundle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-#watch:true
-
-MyApp.app/
-   Contents/
-      Info.plist
-      MacOS/
-      Resources/
